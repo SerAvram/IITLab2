c36db3741edc515f20a89a9f42c621e02e4a7cb5
SerAvram <145595976+SerAvram@users.noreply.github.com>
2024-06-21 21:49:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,34 +1,13 @@
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Определите обработчик команды /start
-def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('Привет! Я ваш бот. Как я могу помочь вам сегодня?')
 
-# Определите обработчик сообщений
-def echo(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text(update.message.text)
+async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f'Hello {update.effective_user.first_name}')
 
-# Определите функцию main
-def main() -> None:
-    # Вставьте сюда ваш токен
-    token = '7493472977:AAGeK-hlJUPhUBpFlSFdO0e0FI1uu-rL5x8'
 
-    # Создайте Updater и передайте ему токен вашего бота
-    updater = Updater(token, use_context=True)
+app = ApplicationBuilder().token("7493472977:AAGeK-hlJUPhUBpFlSFdO0e0FI1uu-rL5x8").build()
 
-    # Получите диспетчера для регистрации обработчиков
-    dp = updater.dispatcher
+app.add_handler(CommandHandler("hello", hello))
 
-    # Зарегистрируйте обработчики
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
-
-    # Запустите бота
-    updater.start_polling()
-
-    # Запускайте бота до тех пор, пока вы не нажмете Ctrl-C или процесс не будет завершен
-    updater.idle()
-
-if name == 'main':
-    main()
+app.run_polling()
